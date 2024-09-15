5876b8bab2df42aaaaec645c1048155577bff560
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-09-15 14:59:22 +0500

    componentdidmount and componentdidupdate done

--- React/codestepbystep/src/Student.jsx
+++ React/codestepbystep/src/Student.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 
-export function Student({ name, email, address }) {
-  return (
-    <div>
-      <h1>Student Data</h1>
-      <p>Student Name: {name}</p>
-      <p>Student Email: {email}</p>
-      <p>Student Address: {address}</p>
-    </div>
-  );
+class Student extends React.Component {
+  constructor(props) {
+    console.log("Student constructor called");
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    console.log("Student componentDidMount called");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Student componentDidUpdate called");
+    console.log("prevState", prevState.count);
+    console.log("currentState", this.state.count);
+  }
+
+  render() {
+    console.log("Student render called");
+    return (
+      <div>
+        <h1>Counter: {this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>Increment</button>
+      </div>
+    )
+  }
 }
+
+export default Student;
