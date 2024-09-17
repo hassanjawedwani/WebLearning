b47e44571de9a5fe544cf22a215e609d1468e236
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-09-18 04:12:49 +0500

    react router learning

--- React/codestepbystep/src/App.js
+++ React/codestepbystep/src/App.js
@@ -1,10 +1,15 @@
-import Student from "./Student";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div>
-      <Student/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
