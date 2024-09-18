b8a88f95615b61e901749bc72aedf5b3f4b33416
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-09-18 05:11:39 +0500

    param learning

--- React/codestepbystep/src/App.js
+++ React/codestepbystep/src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Page404 from "./components/Page404";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/*" element={<Page404 />} />
+        {/* <Route path="/*" element={<Navigate to="/" />} /> */}
       </Routes>
     </BrowserRouter>
   );
