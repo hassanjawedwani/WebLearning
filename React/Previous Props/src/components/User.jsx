e3b5edc7c007e4dab26793d0cb0e451be6d64c3e
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-09-23 05:12:32 +0500

    some minor updates

--- React/Previous Props/src/components/User.jsx
+++ React/Previous Props/src/components/User.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function User({ count }) {
   const lastVal = useRef();
@@ -6,12 +6,34 @@ function User({ count }) {
     lastVal.current = count;
   });
   const prevValue = lastVal.current;
+
   return (
     <div>
       <h1>User Component Count as Props : {count}</h1>
       <h1>User Component Count as PrevProps : {prevValue}</h1>
     </div>
   )
+  
+  
+  // ? why used ref ? let's do with state
+  // const [prevProps, setPreviousProps] = useState();
+  // useEffect(() => {
+  //   setPreviousProps(count); 
+  // });
+  // ! here problem is when we render and then use effect to store value in prevProps it rerender the render function
+  // ! which cause to use show prev Props to 0 instead of nothing ()
+  
+  
+
+  return (
+    <div>
+      <h1>User Component Count as Props : {count}</h1>
+      <h1>User Component Count as PrevProps : {prevProps}</h1>
+    </div>
+  )
+
+
+  
 }
 
 export default User;
