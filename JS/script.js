8daf65a89c46efa5a0b9d487fb7d484ed8c73ffc
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-10-25 17:19:05 +0500

    .at learn

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+const fruits = ["apple", "banana", "mango"];
+console.log(fruits.at(2));
+console.log(fruits[2]);
+console.log(fruits[-1]); // undefined error for negative indexing
+console.log(fruits.at(-1));  // returns "mange"
