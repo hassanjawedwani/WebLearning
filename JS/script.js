71aa23a35bbfdc9a43c216654dc5d24866424537
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-11-21 11:28:21 +0500

    object assign and entries learned

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,26 @@
-const fruits = ["apple", "banana", "mango"];
-console.log(fruits.at(2));
-console.log(fruits[2]);
-console.log(fruits[-1]); // undefined error for negative indexing
-console.log(fruits.at(-1));  // returns "mange"
+const fruits = ["apple", "banana", "mango", "strawberry"];
+
+const ag5883 = {
+  name: "hassan jawed",
+  ag: "2021-ag-5883",
+  semester: 7,
+  married: false
+};
+
+const ag5992 = {
+  name: "ahmad",
+  age: 23,
+  semester: 9
+};
+
+const text = Object.entries(ag5883);
+
+console.log(text);
+
+// for (const property of text) {
+//   console.log(property[0], "\n", property[1]);
+// }
+
+for (const [property, value] of text) {
+  console.log(property, "\n", value);
+}
