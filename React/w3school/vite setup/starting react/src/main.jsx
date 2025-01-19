7d1543bc4824e8c81c450570caead08deacfba07
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2025-01-19 21:25:05 +0500

    react classes, shouldcomponentupdate, consturctor, render, getderivedfromprosps, componentdidmount, render, getsnapshotfrompreverios, render, componendidupadate, componentwillunmount

--- React/w3school/vite setup/starting react/src/main.jsx
+++ React/w3school/vite setup/starting react/src/main.jsx
@@ -65,28 +65,174 @@ import ReactDOM from "react-dom/client";
 //   }
 // }
 
-class Car extends React.Component {
+// class Car extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       color: "blue",
+//     };
+//   }
+//   changeColor = () => {
+//     this.setState({ color: "red" });
+//   }
+
+//   render() {
+//     return <div style={{ backgroundColor: this.state.color }}>hello world<button onClick={this.changeColor}>Change color</button></div>;
+//   }
+// }
+
+// // eslint-disable-next-line react-refresh/only-export-components
+// function Jsx() {
+//   return (
+//     <>
+//       <Car />
+//     </>
+//   );
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<Jsx />);
+
+// class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       favouriteColor: "red"
+//     }
+//   }
+//   // static getDerivedStateFromProps(props, state) {
+//   //   return { favouriteColor: props.favColor };
+//   // }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ favouriteColor: "blue" });
+//     }, 3000);
+//   }
+//   render() {
+//     return (
+//       <h1>My favourite Color is {this.state.favouriteColor}</h1>
+//     )
+//   }
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<Header favColor="yellow" />);
+
+// class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       favouriteColor: "red"
+//     }
+//   }
+//   static getDerivedStateFromProps(props, state) {
+//     console.log("getDerivedFromProps functions called");
+//     return { favouriteColor: props.favColor };
+//   }
+//   render() {
+//     console.log("Render function called")
+//     return (
+//       <div>
+//        <h1>My favourite Color is {this.state.favouriteColor}</h1>
+//         <button onClick={() => { this.setState({ favouriteColor: "purple" }) }}>Click</button>
+//       </div>
+//     )
+//   }
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<Header favColor="yellow" />);
+
+// class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       favouriteColor: "red"
+//     }
+//   }
+//   shouldComponentUpdate() {
+//     return true;
+//   }
+//   render() {
+//     console.log("Render function called")
+//     return (
+//       <div>
+//        <h1>My favourite Color is {this.state.favouriteColor}</h1>
+//         <button onClick={() => { this.setState({ favouriteColor: "purple" }) }}>Click</button>
+//       </div>
+//     )
+//   }
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<Header favColor="yellow" />);
+
+// class Header extends React.Component {
+//   constructor(props) {
+//     console.log("constructor called")
+//     super(props);
+//     this.state = {
+//       favouriteColor: "red"
+//     }
+//   }
+
+//   componentDidMount() {
+//     console.log("componentdidmount called")
+//     setTimeout(() => {
+//       this.setState({favouriteColor: "green"})
+//     }, 3000)
+//   }
+
+//   getSnapshotBeforeUpdate(prevProps, prevState) {
+//     console.log("getSnapshotBeforeUpdate called");
+//     console.log("prev state value is ", prevState.favouriteColor);
+//   }
+
+//   componentDidUpdate() {
+//     console.log("componentDidUpdate called");
+//     console.log("state value", this.state.favouriteColor)
+//   }
+
+//   render() {
+//     console.log("Render function called")
+//     return (
+//       <div>
+//        <h1>My favourite Color is {this.state.favouriteColor}</h1>
+//         <button onClick={() => { this.setState({ favouriteColor: "purple" }) }}>Click</button>
+//       </div>
+//     )
+//   }
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<Header favColor="yellow" />);
+
+class Parent extends React.Component {
   constructor() {
     super();
     this.state = {
-      color: "blue",
+      show: true,
     };
-  }
-  changeColor = () => {
-    this.setState({ color: "red" });
   }
 
   render() {
-    return <div style={{ backgroundColor: this.state.color }}>hello world<button onClick={this.changeColor}>Change color</button></div>;
+    return (
+      <div>
+        {this.state.show && <Child />}
+        <button
+          onClick={() => {
+            this.setState({ show: false });
+          }}
+        >
+          Delete Child
+        </button>
+      </div>
+    );
   }
 }
 
-function Jsx() {
-  return (
-    <>
-      <Car />
-    </>
-  );
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("i am child component is going to unmount");
+  }
+  render() {
+    return <h1>child component</h1>;
+  }
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Jsx />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Parent />);
