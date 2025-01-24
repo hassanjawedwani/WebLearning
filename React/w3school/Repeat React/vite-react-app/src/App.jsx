25c1e7a978037c26ebd5949977aa2d6ade35ccf4
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2025-01-24 16:41:44 +0500

    class class inheritence regular function arrow function learned

---
+++ React/w3school/Repeat React/vite-react-app/src/App.jsx
@@ -0,0 +1,50 @@
+function App() {
+
+  // class Car {
+  //   constructor(name) {
+  //     this.name = name;
+  //   }
+  //   detailCar() {
+  //     return this.name;
+  //   }
+  // }
+
+  // class Model extends Car{
+  //   constructor(name, model) {
+  //     super(name);
+  //     this.model = model;
+  //   }
+  //   detail() {
+  //     return "Car name is : " + this.detailCar() + " Model is : " + this.model;
+  //   }
+  // };
+  
+  // const myCar = new Model("lamborgini", "defender42");
+  // console.log(myCar.detail())
+
+  // const sum = (a, b) => a + b;
+  // console.log(sum(4, 6));
+
+  // class Header {
+  //   constructor() {
+
+  //   }
+  //   changeColor () {
+  //     console.log(this);
+  //   }
+  // }
+  // const myHeader = new Header();
+  // window.addEventListener("load", myHeader.changeColor);
+  
+  // document.getElementById("btn").addEventListener("click", myHeader.changeColor)
+
+  
+
+  return (
+    <div>
+      Hello App
+    </div>
+  )
+}
+
+export default App;
