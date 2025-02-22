64f810d31abc28dae9afb32a0d83e35b92b9f879
HassanJawedWani <hassanjawedwani@gmail.com>
2025-02-22 18:08:21 +0500

    expressjs with Sql learned

--- SQL/Apna College Alpha Badge/index.js
+++ SQL/Apna College Alpha Badge/index.js
@@ -2,7 +2,10 @@ const { faker } = require("@faker-js/faker");
 const mysql = require('mysql2');
 const express = require("express");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 const app = express();
+
 
 
 const port = 3000;
@@ -21,8 +24,13 @@ app.set("views", path.join(__dirname, "./views"))
 app.set("view engine", "ejs");
 
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }));
+
 
 let users = [];
 
@@ -38,12 +46,62 @@ app.get("/", (req, res) => {
   });
 });
 
+app.post("/", (req, res) => {
+  const { username, email, password } = req.body;
+  const user = [
+    uuidv4(),
+    username,
+    email,
+    password
+  ];
+  const newUserSqlQuery = `INSERT INTO user (id, username, email, password) VALUES (?)`;
+  connection.query(newUserSqlQuery, [user], (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+})
+
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.get("/:id/security", (req, res) => {
+  const { id } = req.params;
+  const user = users.find(user => user.id === id);
+  res.render("security.ejs", {user})
+});
+
+
 
 app.get("/:id/edit", (req, res) => {
   const { id } = req.params;
-  const user = users.find(user => user.id === id);
-  console.log(id, user);
-  res.render("edit.ejs", );
+  const query = `SELECT * FROM user WHERE id = ${id}`;
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+    const user = result[0];
+    console.log(user)
+    res.render("edit.ejs", { user });
+  });
+});
+
+app.post("/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const { username } = req.body;
+  const query = "UPDATE user SET username = ? WHERE id = ?";
+  connection.query(query, [username, id], (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.redirect("/");
+  });
+})
+
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deleteUserSqlQuery = `DELETE FROM user WHERE id = '${id}'`;
+  connection.query(deleteUserSqlQuery, (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
+  })
 })
 
 app.listen(port, () => {
