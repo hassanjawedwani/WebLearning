99bdf8dff021eac1557adc1964ad4323ca8dca6a
HassanJawedWani <hassanjawedwani@gmail.com>
2025-03-01 00:12:58 +0500

    middleware learned

--- Express/Apna College Aplha badge/index.js
+++ Express/Apna College Aplha badge/index.js
@@ -2,15 +2,57 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// Middlewares Start
+
+const checkToken = (req, res, next) => {
+  const { token } = req.query;
+  if (token == 233) {
+    next();
+  } else {
+    res.send("please provide right token first");
+  }
+}
+
+
+
+
+// app.use((req, res, next) => {  // logger middleware 
+//   console.log("url:", req.url);
+//   console.log("method:", req.method);
+//   console.log("params:", req.params);
+//   console.log("query:", req.query);
+//   next();
+// })
+
+
+
+
+// Middlewares End
+
+// API Routes Start
 
 app.get("/", (req, res) => {
-  console.log("get request recieved at root path")
-})
+  console.log("hello i'm root");
+  res.send("hello i'm root");
+});
 
 app.get("/search", (req, res) => {
   const query = req.query;
-  console.log(query);
-})
+  res.send(query.query);
+});
+
+app.get("/api", checkToken,  (req, res) => {
+  res.send("Welcome to api page")
+});
+
+app.get("*", (req, res) => {
+  res.send("page 404");
+});
+
+// API Routes End
+
+
+// Server Start
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
