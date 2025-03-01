65e05d92546880bb88604ea26f6505db9c0855c9
HassanJawedWani <hassanjawedwani@gmail.com>
2025-03-02 02:07:58 +0500

    error handling middleware learned

--- MongoDB/Apna College Alpha Badge/mongodb_with_express/index.js
+++ MongoDB/Apna College Alpha Badge/mongodb_with_express/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError");
 
 const app = express();
 const port = 8080;
@@ -27,63 +28,101 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-app.get("/chats", async (req, res) => {
-  const chats = await Chat.find({});
-  res.render("index.ejs", { chats });
+function asyncWrap(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  };
+}
+
+app.get("/", (req, res) => {
+  res.send("Hi i'm root");
 });
+
+app.get(
+  "/chats",
+  asyncWrap(async (req, res, next) => {
+    const chats = await Chat.find({});
+    res.render("index.ejs", { chats });
+  })
+);
 
 app.get("/chats/new", (req, res) => {
   res.render("new.ejs");
 });
 
-app.get("/chats/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const chat = await Chat.findById(id);
-  res.render("edit.ejs", { chat });
-});
-
-app.post("/chats", (req, res) => {
-  const { from, to, message } = req.body;
-  const chat = new Chat({
-    from,
-    to,
-    message,
-    created_at: new Date(),
-  });
-  chat
-    .save()
-    .then((res) => {
-      console.log("added", res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  res.redirect("/chats");
-});
-
-app.put("/chats/:id", (req, res) => {
-  const { id } = req.params;
-  const { from, to, message } = req.body;
-  Chat.findByIdAndUpdate(id, {from, to, message, created_at: new Date()}, {runValidators: true})
-    .then((res) => {
-      console.log("edited", res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  res.redirect("/chats")
-});
-
-app.delete("/chats/:id", (req, res) => {
-  const { id } = req.params;
-  Chat.findByIdAndDelete(id).then(res => {
-    console.log(res);
-  }).catch(err => {
-    console.log(err);
+app.get(
+  "/chats/:id",
+  asyncWrap(async (req, res, next) => {
+    const { id } = req.params;
+    const chat = await Chat.findById(id);
+    if (!chat) {
+      next(new ExpressError("500", "chat not found"));
+    }
+    res.render("show.ejs", { chat });
   })
-  res.redirect("/chats");
-})
+);
 
+app.get(
+  "/chats/:id/edit",
+  asyncWrap(async (req, res) => {
+    const { id } = req.params;
+    const chat = await Chat.findById(id);
+    res.render("edit.ejs", { chat });
+  })
+);
+
+app.post(
+  "/chats",
+  asyncWrap(async (req, res, next) => {
+    const { from, to, message } = req.body;
+    const chat = new Chat({
+      from,
+      to,
+      message,
+      created_at: new Date(),
+    });
+    await chat.save();
+    res.redirect("/chats");
+  })
+);
+
+app.put(
+  "/chats/:id",
+  asyncWrap(async (req, res) => {
+    const { id } = req.params;
+    const { from, to, message } = req.body;
+    await Chat.findByIdAndUpdate(
+      id,
+      { from, to, message, created_at: new Date() },
+      { runValidators: true }
+    );
+    res.redirect("/chats");
+  })
+);
+
+app.delete(
+  "/chats/:id",
+  asyncWrap(async (req, res) => {
+    const { id } = req.params;
+    await Chat.findByIdAndDelete(id);
+    res.redirect("/chats");
+  })
+);
+
+// Error Handling Middlewares
+
+app.use((err, req, res, next) => {
+  console.log(err.name);
+  if (err.name === "ValidationError") {
+    console.log("fuck")
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Some error occured" } = err;
+  res.status(status).send(message);
+});
 
 app.listen(port, () => {
   console.log("App is listening at port ", port);
