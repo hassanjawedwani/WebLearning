369ba03338c5b62d552520468f082d716e366d49
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2025-09-23 01:15:29 +0500

    crud donew

--- Supabase/pedro/src/components/Todo.tsx
+++ Supabase/pedro/src/components/Todo.tsx
@@ -3,28 +3,76 @@ import { todoSchema } from "@/lib/schemas";
 import { TodoForm } from "@/lib/types";
 import { supabase } from "@/supabase-client";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
+interface Task {
+  id: number,
+  title: string,
+  description: string,
+  created_at: string
+}
 const Todo = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [disc, setDisc] = useState<string>("")
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<TodoForm>({ resolver: zodResolver(todoSchema) });
 
+  const fetchTask = async () => {
+    const { data, error } = await supabase.from("todos").select("*").order("title ", {ascending: false});
+    if (error) {
+      toast.error("error in fetcing data");
+      console.log(error);
+    }
+    if (data) {
+      setTasks(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchTask();
+  }, []);
+
   const onSubmit: SubmitHandler<TodoForm> = async (data) => {
     console.log("data", data);
-    const { error } = await supabase.from("todos").insert(data).single();
+    const { data: insertedTask, error } = await supabase.from("todos").insert(data).select().single();
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
+    toast.success("Task added successfully!");
+    if (insertedTask) {
+      setTasks((prevTasks) => [...prevTasks, insertedTask as Task]);
+    }
+  };
+
+  const deleteHandler = async(id: number) => {
+    const { data, error } = await supabase.from("todos").delete().eq("id", id);
 
     if (error) {
       toast.error(error.message);
       return;
     }
+    console.log(data);
+    toast.success("Task deleted successfully");
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== id))
+  }
 
-    toast.success("Task added successfully!");
-  };
+  const updateHandler = async(id: number) => {
+    const { data: updateData, error: updateError }: { data: any; error: any } = await supabase.from("todos").update({description: disc}).eq("id", id);
+
+    if (updateError) {
+      toast.error(updateError.message);
+      return;
+    }
+    console.log(updateData);
+    toast.success("Task updated successfully");
+    setTasks(prevTasks => prevTasks.map(task => task.id === id ? { ...task, description: disc } : task))
+  }
 
   return (
     <div className="w-full h-screen center-flex">
@@ -59,6 +107,17 @@ const Todo = () => {
             Add Task
           </button>
         </form>
+        {tasks?.map((task) => (
+          <div key={task.id}>
+            <h2>title: {task.title}</h2>
+            <p>Description: {task.description}</p>
+            <button onClick={() => deleteHandler(task.id)}>Delete</button>
+            <button onClick={() => updateHandler(task.id)}>Update</button>
+
+          </div>
+        ))}
+
+        <textarea onChange={e => setDisc(e.target.value)}></textarea>
       </div>
     </div>
   );
